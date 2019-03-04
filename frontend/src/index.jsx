407fe60a7c9b3b951f9b12437776366487e7786d
Denis Souza <denyssouza1@hotmail.com>
2019-03-04 19:59:55 -0300

    configurando middleware multi redux-multi

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {applyMiddleware,createStore} from 'redux'
-import {Provider} from 'react-redux'
-import promise from 'redux-promise'
+import React from "react";
+import ReactDOM from "react-dom";
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
 
-import App from './main/app'
-import reducers from './main/reducers'
+import promise from "redux-promise";
+import multi from "redux-multi";
 
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+import App from "./main/app";
+import reducers from "./main/reducers";
 
-const store = applyMiddleware(promise)(createStore)(reducers, devTools)
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = applyMiddleware(multi, promise)(createStore)(reducers, devTools);
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App/>
-    </Provider>
-    ,document.getElementById('app'))
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -4,7 +4,7 @@ import { bindActionCreators } from 'redux'
 
 import Grid from '../template/grid'
 import IconButton from '../template/iConButton'
-import { changeDescription, search } from './todoActions'
+import { changeDescription, search,add } from './todoActions'
 
 
 class TodoForm extends Component {
@@ -18,14 +18,18 @@ class TodoForm extends Component {
     }
 
     keyHandler(e) {
+        const {add, search, description} = this.props
+
         if (e.key === 'Enter') {
-            e.shiftKey ? this.props.handleSearch() : this.props.handleAdd()
+            e.shiftKey ? search() : add(description)
         } else if (e.key === 'Escape') {
             props.handleClear()
         }
     }
 
     render() {
+        const {add, search, description} = this.props
+
         return (
             <div role="form" className='todoForm row'>
                 <Grid cols='12 10 10'>
@@ -35,8 +39,8 @@ class TodoForm extends Component {
                         value={this.props.description} />
                 </Grid>
                 <Grid cols='12 2 2'>
-                    <IconButton style='primary' icon='plus' onClick={this.props.handleAdd}></IconButton>
-                    <IconButton style='info' icon='search' onClick={this.props.handleSearch}></IconButton>
+                    <IconButton style='primary' icon='plus' onClick={() => add(description)}></IconButton>
+                    <IconButton style='info' icon='search' onClick={()=> search()}></IconButton>
                     <IconButton style='danger' icon='close' onClick={this.props.handleClear}></IconButton>
                 </Grid>
             </div>
@@ -46,6 +50,6 @@ class TodoForm extends Component {
 
 const mapStateToProps = state => ({ description: state.todo.description })
 
-const mapDispatchToProps = dispatch => (bindActionCreators({ changeDescription, search }, dispatch))
+const mapDispatchToProps = dispatch => (bindActionCreators({ changeDescription, search, add }, dispatch))
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
